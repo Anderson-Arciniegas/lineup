403098ed2122942037d8177dd3167980c8acebdd
OmarJr11 <omaryoeljr@gmail.com>
2025-07-13 01:18:48 -0400

    :hammer: main feat(components) business data component

--- libs/shared/ui/src/lib/business-data/business-data.ts
+++ libs/shared/ui/src/lib/business-data/business-data.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TagModule } from 'primeng/tag';
+import { RippleModule } from 'primeng/ripple';
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'lib-business-data',
-  imports: [CommonModule, TagModule],
+  imports: [CommonModule, TagModule, RippleModule, CardModule, DividerModule],
   templateUrl: './business-data.html',
   styleUrl: './business-data.scss',
 })
