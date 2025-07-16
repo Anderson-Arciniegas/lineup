b835905371323895c80881f5ead2715a147baaca
OmarJr11 <omaryoeljr@gmail.com>
2025-07-15 20:45:57 -0400

    :hammer: main feat(Business) Business view

--- libs/shared/ui/src/lib/ui/ui.ts
+++ libs/shared/ui/src/lib/ui/ui.ts
@@ -2,9 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CategoriesList } from '../categories-list/categories-list';
 import { BusinessData } from '../business-data/business-data';
+import { FormsModule } from '@angular/forms';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { Button } from '../button/button';
+import { SkeletonModule } from 'primeng/skeleton';
+
 @Component({
   selector: 'lib-ui',
-  imports: [CommonModule, CategoriesList, BusinessData],
+  imports: [
+    CommonModule,
+    CategoriesList,
+    BusinessData,
+    FormsModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    Button,
+    SkeletonModule
+  ],
   templateUrl: './ui.html',
   styleUrl: './ui.css',
 })
@@ -18,4 +35,5 @@ export class Ui {
     { id: 6, name: 'Accesorios' },
     { id: 7, name: 'Ropa Interior' },
   ];
+  value: '';
 }
