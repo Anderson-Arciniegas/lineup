6e8015dbaa2be15c6464f30e95a95962a651b26c
OmarJr11 <omaryoeljr@gmail.com>
2025-07-16 23:55:00 -0400

    :hammer: CU-86c4hyj0d feat(Product) Create view for product

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -4,4 +4,5 @@ export * from './lib/ui/ui';
 export * from './lib/categories-list/categories-list';
 export * from './lib/category-item/category-item';
 export * from './lib/business-data/business-data';
-
+export * from './lib/product-breadcrumb/product-breadcrumb';
+export * from './lib/product-info/product-info';
