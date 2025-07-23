acfc1a12fb32e930cfd29e342aa4b9b10877851e
Anderson Arciniegas <anderarci1210@gmail.com>
2025-07-23 09:37:18 -0400

    :sparkles: Feature/CU-86c4hff2n feat(app) updated dependencies

--- apps/lineup/src/app/features/business/views/product-page/product-page.spec.ts
+++ apps/lineup/src/app/features/business/views/product-page/product-page.spec.ts
@@ -1,4 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // For asynchronous animations
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 import { ProductPage } from './product-page';
 
 describe('ProductPage', () => {
@@ -7,7 +10,15 @@ describe('ProductPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductPage],
+      imports: [ProductPage, TranslateModule.forRoot(),],
+            providers: [{
+                    provide: ActivatedRoute,
+                    useValue: {}
+                },
+                TranslateService,
+                TranslateStore,
+                provideAnimationsAsync(),
+            ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductPage);
