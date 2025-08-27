41e252fa0d4c3c9cdc6392a7e60337e4b9feb183
Anderson Arciniegas <anderarci1210@gmail.com>
2025-08-27 18:12:02 -0400

    :sparkles: Feature/CU-86c4qx7x9 feat(mobile) create app mobile

---
+++ apps/mobile/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomePage } from './features/home/views/home-page/home-page';
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+];
