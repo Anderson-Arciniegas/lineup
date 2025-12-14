d326a5ac2d1b11d84cba950039a3416e14dfeb78
OmarJr11 <omaryoeljr@gmail.com>
2025-12-14 19:14:50 -0400

    :hammer: auth-guard feat(Guard) Fix auth guard

--- apps/lineup/src/environment/environment.ts
+++ apps/lineup/src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  userApi: 'https://190.9.40.168/api1/graphql',
-  businessApi: 'https://190.9.40.168/api3/graphql',
+  userApi: 'http://localhost:3000/graphql',
+  businessApi: 'http://localhost:3002/graphql',
   crypto: {
     seed: 'ThisIsTheDevSeed',
     secret: 'a7f82e39372e2f31b878c3971cac81d04bcff42ad9538131418d6d5e1047f04d',
